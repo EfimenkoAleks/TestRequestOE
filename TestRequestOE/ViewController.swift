61e55c1764bcd6192e16cbd790ea359f5fa02c56
Oleksandr Efimenko <oefimenko@netfully.com>
2025-05-02 11:42:02 +0300

    Initial Commit

---
+++ TestRequestOE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestRequestOE
+//
+//  Created by Aleksandr on 02.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
